bbab11435fda048049c1bda55d07d643422bd8e7
Simone Messina <simone.messina@emearasoft.com>
2017-02-14 17:25:54 +0100

    Ottimizzazione DELELE_HISTORY_WORKITEM

--- properties/src/main/resources/deletesirehistoryworkitem.sql
+++ properties/src/main/resources/deletesirehistoryworkitem.sql
@@ -1,16 +1,14 @@
-delete from DMALM_SIRE_HISTORY_WORKITEM 
-where (data_caricamento <= ? 
-and sire_history_workitem_pk not in(
-  select hw.sire_history_workitem_pk 
-  from DMALM_SIRE_HISTORY_WORKITEM hw
-  join (select c_type, max(c_rev) as max_c_rev
-    from DMALM_SIRE_HISTORY_WORKITEM
-    group by c_type) mx 
-  on mx.c_type = hw.c_type and mx.max_c_rev = hw.c_rev)
-and sire_history_workitem_pk not in(
-    select st.sire_history_workitem_pk 
-    from DMALM_SIRE_HISTORY_WORKITEM st 
-    where st.C_STATUS in('chiuso', 'chiuso_falso', 'in_esercizio', 'in_esecuzione', 'completo', 'eseguito')) 
-and not c_type = 'defect' 
-) 
-or data_caricamento = ? 
+DELETE
+FROM DMALM_SIRE_HISTORY_WORKITEM
+WHERE data_caricamento = ?
+  OR (data_caricamento <= ?
+      AND C_STATUS not in('chiuso', 'chiuso_falso', 'in_esercizio', 'in_esecuzione', 'completo', 'eseguito'))
+      AND NOT c_type = 'defect'
+      AND sire_history_workitem_pk NOT in
+         ( SELECT hw.sire_history_workitem_pk
+          FROM DMALM_SIRE_HISTORY_WORKITEM hw
+          JOIN
+            (SELECT c_type, max(c_rev) AS max_c_rev
+             FROM DMALM_SIRE_HISTORY_WORKITEM
+             GROUP BY c_type) mx ON mx.c_type = hw.c_type
+          AND mx.max_c_rev = hw.c_rev))
