22282d2ba38dcdc3e9b0d5fe3a51996084dcab42
Daniele Cortis <daniele.cortis@profesia.it>
2017-10-27 16:55:47 +0200

    Risolvo conflitto DmalmDifettoProdotto

--- jar/src/main/java/lispa/schedulers/bean/target/fatti/DmalmDifettoProdotto.java
+++ jar/src/main/java/lispa/schedulers/bean/target/fatti/DmalmDifettoProdotto.java
@@ -88,6 +88,9 @@ public class DmalmDifettoProdotto {
 	//DM_ALM-223
 	private java.sql.Timestamp dtDisponibilita;
 	
+	//DM_ALM-320
+	private String priority;
+	
     public java.sql.Timestamp getDtDisponibilita() {
 		return dtDisponibilita;
 	}
@@ -316,14 +319,11 @@ public class DmalmDifettoProdotto {
     	//DM_ALM-289 Aggiunto commento # 4
     	if(provenienzaDifetto!=null)
     	{
-    		provenienzaDifetto=provenienzaDifetto.trim();
+    		provenienzaDifetto=provenienzaDifetto.split(" ")[0];
 	    	if(provenienzaDifetto.contains("SVI"))
 	    		provenienzaDifetto.replaceAll("SVI", "SV");
     	}
         this.provenienzaDifetto = provenienzaDifetto;
-    	if(provenienzaDifetto.contains("SVI"))
-    		provenienzaDifetto.replaceAll("SVI", "SV");
-        this.provenienzaDifetto = provenienzaDifetto.trim();
     }
 
     public Double getRankStatoDifetto() {
@@ -412,6 +412,14 @@ public class DmalmDifettoProdotto {
 
 	public void setAnnullato(String annullato) {
 		this.annullato = annullato;
+	}
+
+	public String getPriority() {
+		return priority;
+	}
+
+	public void setPriority(String priority) {
+		this.priority = priority;
 	}
 
 }
