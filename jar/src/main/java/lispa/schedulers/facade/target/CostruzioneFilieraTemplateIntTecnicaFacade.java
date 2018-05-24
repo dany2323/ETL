226f8c5ab4b71225bd1ecf738991d35bb3c8623a
Daniele Cortis <daniele.cortis@profesia.it>
2018-05-24 08:47:32 +0200

    Fix Tabelle template

--- jar/src/main/java/lispa/schedulers/facade/target/CostruzioneFilieraTemplateIntTecnicaFacade.java
+++ jar/src/main/java/lispa/schedulers/facade/target/CostruzioneFilieraTemplateIntTecnicaFacade.java
@@ -73,21 +73,6 @@ public class CostruzioneFilieraTemplateIntTecnicaFacade {
 				idFiliera = gestisciLista(idFiliera, startWorkitemsList,
 						insertedWorkitemsList);
 			}
-
-			// INIZIO DM_ALM-352
-			// aggiunta dei WI Build alla tabella DMALM_TEMPLATE_INT_TECNICA
-			List<DmalmLinkedWorkitems> insertedWorkitemsListBuild = new LinkedList<DmalmLinkedWorkitems>();
-			
-			List<DmalmLinkedWorkitems> startWorkitemsListBuild = LinkedWorkitemsDAO
-					.getStartWorkitemsAddBuildTemplate(dataInizioFiliera);
-			logger.debug("CostruzioneFilieraTemplateIntTecnicaFacade - Aggiunta WI Build - lista.size: "
-					+ startWorkitemsListBuild.size());
-
-			if (startWorkitemsListBuild.size() > 0) {
-				idFiliera = gestisciListaAddWiBuild(idFiliera, startWorkitemsListBuild,
-						insertedWorkitemsListBuild);
-			}
-			// FINE DM_ALM-352
 			
 			logger.info("STOP CostruzioneFilieraTemplateIntTecnicaFacade.execute()");
 		} catch (DAOException de) {
