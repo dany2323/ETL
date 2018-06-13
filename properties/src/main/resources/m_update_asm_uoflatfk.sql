bc445b2a156a56c4cf4e1c6c1d1a923e89baf77e
Daniele Cortis <daniele.cortis@profesia.it>
2018-06-13 12:20:59 +0200

    Fix Uo flat ASM

--- properties/src/main/resources/m_update_asm_uoflatfk.sql
+++ properties/src/main/resources/m_update_asm_uoflatfk.sql
@@ -5,7 +5,7 @@ set p.DMALM_UNITAORG_FLAT_FK_04 =
         (select MAX(uo.ID_FLAT)
         from DMALM_EL_UNITA_ORGANIZZAT_FLAT uo
         where uo.UNITA_ORG_FK_08 = p.DMALM_UNITAORGANIZZATIVA_FK_03
-        and p.dt_inizio_validita between uo.DT_INIZIO_VALIDITA and uo.DT_FINE_VALIDITA) 
+        ) 
 where p.DMALM_UNITAORG_FLAT_FK_04 is null 
 and p.DMALM_UNITAORGANIZZATIVA_FK_03 is not null; 
 update dmalm_asm p  
@@ -13,7 +13,7 @@ set p.DMALM_UNITAORG_FLAT_FK_04 =
         (select MAX(uo.ID_FLAT)
         from DMALM_EL_UNITA_ORGANIZZAT_FLAT uo
         where uo.UNITA_ORG_FK_07 = p.DMALM_UNITAORGANIZZATIVA_FK_03
-        and p.dt_inizio_validita between uo.DT_INIZIO_VALIDITA and uo.DT_FINE_VALIDITA) 
+        ) 
 where p.DMALM_UNITAORG_FLAT_FK_04 is null 
 and p.DMALM_UNITAORGANIZZATIVA_FK_03 is not null; 
 update dmalm_asm p  
@@ -21,7 +21,7 @@ set p.DMALM_UNITAORG_FLAT_FK_04 =
         (select MAX(uo.ID_FLAT)
         from DMALM_EL_UNITA_ORGANIZZAT_FLAT uo
         where uo.UNITA_ORG_FK_06 = p.DMALM_UNITAORGANIZZATIVA_FK_03
-        and p.dt_inizio_validita between uo.DT_INIZIO_VALIDITA and uo.DT_FINE_VALIDITA) 
+        ) 
 where p.DMALM_UNITAORG_FLAT_FK_04 is null 
 and p.DMALM_UNITAORGANIZZATIVA_FK_03 is not null; 
 update dmalm_asm p  
@@ -29,7 +29,7 @@ set p.DMALM_UNITAORG_FLAT_FK_04 =
         (select MAX(uo.ID_FLAT)
         from DMALM_EL_UNITA_ORGANIZZAT_FLAT uo
         where uo.UNITA_ORG_FK_05 = p.DMALM_UNITAORGANIZZATIVA_FK_03
-        and p.dt_inizio_validita between uo.DT_INIZIO_VALIDITA and uo.DT_FINE_VALIDITA) 
+        ) 
 where p.DMALM_UNITAORG_FLAT_FK_04 is null 
 and p.DMALM_UNITAORGANIZZATIVA_FK_03 is not null; 
 update dmalm_asm p  
@@ -37,7 +37,7 @@ set p.DMALM_UNITAORG_FLAT_FK_04 =
         (select MAX(uo.ID_FLAT)
         from DMALM_EL_UNITA_ORGANIZZAT_FLAT uo
         where uo.UNITA_ORG_FK_04 = p.DMALM_UNITAORGANIZZATIVA_FK_03
-        and p.dt_inizio_validita between uo.DT_INIZIO_VALIDITA and uo.DT_FINE_VALIDITA) 
+        ) 
 where p.DMALM_UNITAORG_FLAT_FK_04 is null 
 and p.DMALM_UNITAORGANIZZATIVA_FK_03 is not null; 
 update dmalm_asm p  
@@ -45,7 +45,7 @@ set p.DMALM_UNITAORG_FLAT_FK_04 =
         (select MAX(uo.ID_FLAT)
         from DMALM_EL_UNITA_ORGANIZZAT_FLAT uo
         where uo.UNITA_ORG_FK_03 = p.DMALM_UNITAORGANIZZATIVA_FK_03
-        and p.dt_inizio_validita between uo.DT_INIZIO_VALIDITA and uo.DT_FINE_VALIDITA) 
+        ) 
 where p.DMALM_UNITAORG_FLAT_FK_04 is null 
 and p.DMALM_UNITAORGANIZZATIVA_FK_03 is not null; 
 update dmalm_asm p  
@@ -53,7 +53,7 @@ set p.DMALM_UNITAORG_FLAT_FK_04 =
         (select MAX(uo.ID_FLAT)
         from DMALM_EL_UNITA_ORGANIZZAT_FLAT uo
         where uo.UNITA_ORG_FK_02 = p.DMALM_UNITAORGANIZZATIVA_FK_03
-        and p.dt_inizio_validita between uo.DT_INIZIO_VALIDITA and uo.DT_FINE_VALIDITA) 
+        ) 
 where p.DMALM_UNITAORG_FLAT_FK_04 is null 
 and p.DMALM_UNITAORGANIZZATIVA_FK_03 is not null; 
 update dmalm_asm p  
@@ -61,7 +61,7 @@ set p.DMALM_UNITAORG_FLAT_FK_04 =
         (select MAX(uo.ID_FLAT)
         from DMALM_EL_UNITA_ORGANIZZAT_FLAT uo
         where uo.UNITA_ORG_FK_01 = p.DMALM_UNITAORGANIZZATIVA_FK_03
-        and p.dt_inizio_validita between uo.DT_INIZIO_VALIDITA and uo.DT_FINE_VALIDITA) 
+        ) 
 where p.DMALM_UNITAORG_FLAT_FK_04 is null 
 and p.DMALM_UNITAORGANIZZATIVA_FK_03 is not null; 
 update dmalm_asm p  
