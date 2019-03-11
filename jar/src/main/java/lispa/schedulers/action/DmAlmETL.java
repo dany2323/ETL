085101abd0385d5b0cb0f4ca3494ef8cac0cb79d
Luca <luca.porro@emerasoft.com>
2019-03-11 10:16:46 +0100

    Fix rollback

--- jar/src/main/java/lispa/schedulers/action/DmAlmETL.java
+++ jar/src/main/java/lispa/schedulers/action/DmAlmETL.java
@@ -141,7 +141,7 @@ public class DmAlmETL {
 			}
 		}
 		// se errore nella Stored Procedure effettuo il ripristino di tutto
-		if (ErrorManager.getInstance().hasError()) {
+		if (ErrorManager.getInstance().hasError() && !RecoverManager.getInstance().isRecovered()) {
 			// SFERA/ELETTRA/SGRCM
 			if (ExecutionManager.getInstance().isExecutionSfera()
 					|| ExecutionManager.getInstance()
