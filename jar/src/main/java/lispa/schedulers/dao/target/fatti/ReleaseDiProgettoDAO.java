3010eff8d5d5b93319246770c2db13b0485df541
Luca <luca.porro@emerasoft.com>
2019-05-30 14:28:56 +0200

    Ripristino ReleaseDiProgetto

--- jar/src/main/java/lispa/schedulers/dao/target/fatti/ReleaseDiProgettoDAO.java
+++ jar/src/main/java/lispa/schedulers/dao/target/fatti/ReleaseDiProgettoDAO.java
@@ -45,10 +45,9 @@ public class ReleaseDiProgettoDAO {
 		Connection connection = null;
 
 		DmalmReleaseDiProgetto bean = null;
-//		List<DmalmReleaseDiProgetto> releases = new ArrayList<DmalmReleaseDiProgetto>(
-//				QueryUtils.getCountByWIType(Workitem_Type.EnumWorkitemType.release));
+		List<DmalmReleaseDiProgetto> releases = new ArrayList<DmalmReleaseDiProgetto>(
+				QueryUtils.getCountByWIType(Workitem_Type.EnumWorkitemType.release));
 
-		List<DmalmReleaseDiProgetto> releases = new ArrayList<DmalmReleaseDiProgetto>();
 		try {
 			cm = ConnectionManager.getInstance();
 			connection = cm.getConnectionOracle();
