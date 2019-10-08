5da9f1272d425f5f9b5b114a17e2a4a825fee547
Luca <luca.porro@emerasoft.com>
2019-10-08 11:27:30 +0200

    Modifica aggiornamento project-flatUO

--- properties/src/main/resources/m_update_project_uoflatfk.sql
+++ properties/src/main/resources/m_update_project_uoflatfk.sql
@@ -12,9 +12,9 @@ update dmalm_project p
 set p.DMALM_UNITAORG_FLAT_FK_04 = 
         (select MAX(uo.ID_FLAT)
         from DMALM_EL_UNITA_ORGANIZZAT_FLAT uo
-        where uo.UNITA_ORG_FK_15 = p.DMALM_UNITAORGANIZZATIVA_FK_03
+        where uo.UNITA_ORG_FK_14 = p.DMALM_UNITAORGANIZZATIVA_FK_03
         and p.dt_inizio_validita between uo.DT_INIZIO_VALIDITA and uo.DT_FINE_VALIDITA
-        and uo.UNITA_ORG_FK_14='0') 
+        and uo.UNITA_ORG_FK_15='0') 
 where p.DMALM_UNITAORG_FLAT_FK_04 is null 
 and p.DMALM_UNITAORGANIZZATIVA_FK_03 is not null; 
 update dmalm_project p 
